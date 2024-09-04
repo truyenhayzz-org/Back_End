8b90363cb7d07661a1d3cc82cf41ceab246f85d0
truongphan4898 <truongphan4898@gmail.com>
2024-09-04 15:23:44 +0700

    first commit

---
+++ Documents/foder/truyenhay_backend/src/main/java/com/example/truyenhay_backend/TruyenhayBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.truyenhay_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TruyenhayBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TruyenhayBackendApplication.class, args);
+    }
+
+}
---
+++ Documents/foder/truyenhay_backend/src/main/java/com/example/truyenhay_backend/model/User.java
@@ -0,0 +1,24 @@
+package com.example.truyenhay_backend.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.boot.autoconfigure.web.WebProperties;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+}
